d2f8afb578662131e0b9811fb2493ef89dcb290a
Mkupr <mkupr@outlook.com>
2023-02-15 01:28:22 +0300

    import sum_two_numbers and call it

---
+++ main.py
@@ -0,0 +1,7 @@
+from funcs import sum_two_numbers
+
+first_numer = int(input('Введите первое число'))
+second_numer = int(input('Введите второе число'))
+result = sum_two_numbers(first_numer, second_numer)
+
+
