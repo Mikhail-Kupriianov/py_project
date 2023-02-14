dd18f35299bf541242f614199b07efe85faec2d6
Mkupr <mkupr@outlook.com>
2023-02-15 01:30:19 +0300

    import minus_two_numbers and call it

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from funcs import sum_two_numbers
+from funcs import sum_two_numbers, minus_two_numbers
 
 first_numer = int(input('Введите первое число'))
 second_numer = int(input('Введите второе число'))
 result = sum_two_numbers(first_numer, second_numer)
+
+first_numer = int(input('Введите первое число'))
+second_numer = int(input('Введите второе число'))
+result = minus_two_numbers(first_numer, second_numer)
 
 
